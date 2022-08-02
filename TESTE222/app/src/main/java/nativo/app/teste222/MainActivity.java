71744181386f0450e758c07cea34bbcc87ddd16d
arielsam567 <Saidemim112104>
2022-08-02 16:41:46 -0300

    update

--- TESTE222/app/src/main/java/nativo/app/teste222/MainActivity.java
+++ TESTE222/app/src/main/java/nativo/app/teste222/MainActivity.java
@@ -6,20 +6,43 @@ import android.view.View;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.view.FlutterMain;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
 public class MainActivity extends AppCompatActivity {
+    public FlutterEngine flutterEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = new FlutterEngine(this);
+
+        // Configure an initial route.
+        flutterEngine.getNavigationChannel().setInitialRoute("/");
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+        // Cache the FlutterEngine to be used by FlutterActivity or FlutterFragment.
+        FlutterEngineCache
+                .getInstance()
+                .put("my_engine_id", flutterEngine);
     }
 
     public void onButtonClick(View view) {
         System.out.println("CLICK ffff FFFF");
         startActivity(
-                FlutterActivity.createDefaultIntent(this)
+                ///DEMORA PARA ABRIR A NOVA TELA
+                //FlutterActivity.createDefaultIntent(this)
+
+                ///ABRE INSTANTANEAMENTE A NOVA TELA
+                FlutterActivity
+                        .withCachedEngine("my_engine_id")
+                        .build(this)
         );
     }
 }
