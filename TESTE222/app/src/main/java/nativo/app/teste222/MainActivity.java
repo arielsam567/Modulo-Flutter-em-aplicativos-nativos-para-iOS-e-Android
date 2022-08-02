d016656905e3e7ea7cc06fbb57e5cb3d90aa26b7
arielsam567 <Saidemim112104>
2022-08-02 14:32:24 -0300

    update

--- TESTE222/app/src/main/java/nativo/app/teste222/MainActivity.java
+++ TESTE222/app/src/main/java/nativo/app/teste222/MainActivity.java
@@ -2,6 +2,11 @@ package nativo.app.teste222;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.view.FlutterMain;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        System.out.println("CLICK ffff FFFF");
+        startActivity(
+                FlutterActivity.createDefaultIntent(this)
+        );
     }
 }
